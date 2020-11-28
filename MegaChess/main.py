b981d4c3826934eebfb9917fb5edb8f2b512c816
Luciano Olivera <oliveralucianof@gmail.com>
2020-11-28 13:52:31 -0300

    Add new functionality:    -FactoryEvent

--- MegaChess/main.py
+++ MegaChess/main.py
@@ -1,12 +1,11 @@
 import asyncio
-import json
 import websockets
 import json
+from factoryEvent import FactoryEvent
 
-from game import Game
 
 
-async def send(websocket, action, data):
+async def send(websocket,action,data):
     message = json.dumps(
         {
             'action': action,
@@ -17,53 +16,24 @@ async def send(websocket, action, data):
     await websocket.send(message)
 
 
-async def start (token):
+async def start(token):
     uri = "ws://megachess.herokuapp.com/service?authtoken={}".format(token)
     async with websockets.connect(uri) as websocket:
-        messageEvent = {}
         while True:
             try:
                 response = await websocket.recv()
                 data = json.loads(response)
+                my_factory = FactoryEvent()
                 if data['event'] == 'update_user_list':
-                    print(f"< {response}")
+                    my_factory.get_event('update_user_list',data)
                 if data['event'] == 'gameover':
-                    print(f"< {response}")
+                    my_factory.get_event('gameover',data)
                 if data['event'] == 'ask_challenge':
-                    print(f"< {response}")
-                    board_id = data['data']['board_id']
-                    messageEvent['action'] = 'accept_challenge'
-                    messageEvent['board_id'] = board_id
-                    print(f'message{messageEvent}')
-                    await send(websocket, messageEvent['action'], {'board_id': messageEvent['board_id'], }, )
+                    messageEvent = my_factory.get_event('ask_challenge',data)
+                    await send(websocket,messageEvent['action'],{'board_id': messageEvent['board_id'],},)
                 if data['event'] == 'your_turn':
-                    print(f"< {response}")
-                    messageEvent['action'] = 'move'
-                    messageEvent['data']['board_id'] = data['data']['board_id']
-                    messageEvent['data']['turn_token'] = data['data']['turn_token']
-                    boardTurn = data['data']['board']
-                    colorTurn = data['data']['actual_turn']
-                    moveleft = data['data']['move_left']
-                    turntoken = data['data']['turn_token']
-                    # crear un game con boardTurn colorTurn
-                    game = Game(boardTurn, turntoken, moveleft)
-                    game.definestrategy(colorTurn)
-
-
-
-                    await send(
-                        websocket,
-                        'move',
-                        {
-                            'board_id': data['data']['board_id'],
-                            'turn_token': data['data']['turn_token'],
-                            'from_row': "",
-                            'from_col': "",
-                            'to_row': "",
-                            'to_col': "",
-                            },
-                        )
-
+                    my_factory.get_event('your_turn',data)
+                    await send(  websocket, 'move', {'board_id': data['data']['board_id'],'turn_token': data['data']['turn_token'], 'from_row': "",'from_col': "", 'to_row': "",'to_col': "", }, )
             except Exception as e:
                 print('retry')
 
