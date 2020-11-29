102566fb2a57b6e87331efda48952c9374ad850e
Luciano Olivera <oliveralucianof@gmail.com>
2020-11-29 11:16:17 -0300

    New factory Board Pieces:     -Class Factory:     	   -get_BoardPieces()
         -Class MoveScore:
         	    -moveScore()
    	    -highScore()

---
+++ MegaChess/piecesStrategy.py
@@ -0,0 +1,46 @@
+from const import King,Rook,Queen,Pawn,Bishop,Knight,Empty
+from pieceValue import PieceValue
+
+
+class PiecesStrategy(object):
+    def __init__(self,board):
+        self.board = board
+
+    def moves(self):
+        raise NotImplementedError
+
+    def movesAtack(self):
+        raise NotImplementedError
+
+
+class MoveScore():
+    def moveScore(self,board,name):
+        if board.get_name() is 'p' or board.get_name() is 'P':
+            scorePiece = PieceValue().value(name)
+            return Pawn + scorePiece
+        elif board.get_name() is 'h' or board.get_name() is 'H':
+            scorePiece = PieceValue().value(name)
+            return Knight + scorePiece
+        elif board.get_name() is 'q' or board.get_name() is 'q':
+            scorePiece = PieceValue().value(name)
+            return Queen + scorePiece
+        elif board.get_name() is 'r' or board.get_name() is 'R':
+            scorePiece = PieceValue().value(name)
+            return Rook + scorePiece
+        elif board.get_name() is 'b' or board.get_name() is 'b':
+            scorePiece = PieceValue().value(name)
+            return Bishop + scorePiece
+        elif board.get_name() is 'K' or board.get_name() is 'K':
+            scorePiece = PieceValue().value(name)
+            return Knight + scorePiece
+
+    def highScore(self,listMovesScore):
+        highScore = 0
+        moveHighScore = []
+        for i,c in enumerate(listMovesScore):
+            for w,j in enumerate(c):
+               if j[4] > highScore:
+                   highScore = j[4]
+                   moveHighScore = j
+        return moveHighScore
+
