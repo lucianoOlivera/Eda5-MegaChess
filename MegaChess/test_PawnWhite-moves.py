17811ea139ac40fc66d47ac8bc4df386587bcacc
Luciano Olivera <oliveralucianof@gmail.com>
2020-12-03 22:13:23 -0300

    New Test Pawn White: 		-test_PawnWhiteMoves 		-test_PawnWhiteMovesAtack

--- MegaChess/test_PawnWhite-moves.py
+++ MegaChess/test_PawnWhite-moves.py
@@ -1,20 +1,20 @@
 import pytest
 
-from blackStrategy import PawnBlack
 from board import Board
+from whiteStrategy import PawnWhite
 
 board = (      'rrhhbbqqkkbbhhrr'
                'rrhhbbbqkkbbhhrr'
                'pppppppppppppppp'
                'pppppppppppppppp'
-               'rrrrrrrrrrrrrrrr'
-               '                '
-               '       p        '
                '                '
                '                '
                '                '
                '                '
+               '     p p        '
+               '      P         '
                '                '
+               'RRRRRRRRRRRRRRRR'
                'PPPPPPPPPPPPPPPP'
                'PPPPPPPPPPPPPPPP'
                'RRHHBBQQKKBBHHRR'
@@ -47,7 +47,7 @@ boardNone=  (  'rrhhbbqqkkbbhhrr'
                '                '
                '                '
                '                '
-               '                '
+               'RRRRRRRRRRRRRRRR'
                'PPPPPPPPPPPPPPPP'
                'PPPPPPPPPPPPPPPP'
                'RRHHBBQQKKBBHHRR'
@@ -58,9 +58,9 @@ boardGameNone = Board(boardNone).boardGame()
 
 @pytest.mark.parametrize(
    "imput,expected",[
-            (boardGame,[[6, 7, 7, 7, 10]]),
-            (boardGameStart,[[3, 0, 4, 0, 10], [3, 0, 5, 0, 10], [3, 1, 4, 1, 10], [3, 1, 5, 1, 10], [3, 2, 4, 2, 10], [3, 2, 5, 2, 10], [3, 3, 4, 3, 10], [3, 3, 5, 3, 10], [3, 4, 4, 4, 10], [3, 4, 5, 4, 10], [3, 5, 4, 5, 10], [3, 5, 5, 5, 10], [3, 6, 4, 6, 10], [3, 6, 5, 6, 10], [3, 7, 4, 7, 10], [3, 7, 5, 7, 10], [3, 8, 4, 8, 10], [3, 8, 5, 8, 10], [3, 9, 4, 9, 10], [3, 9, 5, 9, 10], [3, 10, 4, 10, 10], [3, 10, 5, 10, 10], [3, 11, 4, 11, 10], [3, 11, 5, 11, 10], [3, 12, 4, 12, 10], [3, 12, 5, 12, 10], [3, 13, 4, 13, 10], [3, 13, 5, 13, 10], [3, 14, 4, 14, 10], [3, 14, 5, 14, 10], [3, 15, 4, 15, 10], [3, 15, 5, 15, 10]]),
-            (boardGameNone,[])
+            (boardGameNone,[]),
+            (boardGameStart,[[12, 0, 11, 0, 10], [12, 1, 11, 1, 10], [12, 2, 11, 2, 10], [12, 3, 11, 3, 10], [12, 4, 11, 4, 10], [12, 5, 11, 5, 10], [12, 6, 11, 6, 10], [12, 7, 11, 7, 10], [12, 8, 11, 8, 10], [12, 9, 11, 9, 10], [12, 10, 11, 10, 10], [12, 11, 11, 11, 10], [12, 12, 11, 12, 10], [12, 13, 11, 13, 10], [12, 14, 11, 14, 10], [12, 15, 11, 15, 10]]),
+            (boardGame,[[9, 6, 8, 6, 10]])
        ])
-def test_PawnBlackMoves(imput,expected):
-    assert PawnBlack(imput).moves() == expected
+def test_PawnWhiteMoves(imput,expected):
+    assert PawnWhite(imput).moves() == expected
