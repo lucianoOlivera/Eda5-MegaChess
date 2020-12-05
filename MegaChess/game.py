6a7702f72e5e97cd7b0310fd6a833333637616b1
Luciano Olivera <oliveralucianof@gmail.com>
2020-12-05 18:43:11 -0300

    fix bug reference

--- MegaChess/game.py
+++ MegaChess/game.py
@@ -1,4 +1,3 @@
-import numpy as np
 from strategyColour import Context
 from board import Board
 from blackStrategy import BlackStrategy
@@ -6,7 +5,7 @@ from whiteStrategy import WhiteStrategy
 
 
 class Game():
-    def __init__(self,board_id,turn_token,move_left):
+    def __init__(self,board_id,turn_token,move_left) -> object:
         self.board_id = board_id
         self.turn_token = turn_token
         self.move_left = move_left
@@ -24,3 +23,8 @@ class Game():
         return result
 
 
+boardStart ='rrhhbbqqkkbbhhrrrrhhbbqqkkbbhhrrp ppppppppppppppp pppppppppppppp p               p                                                                               P                              P PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPRRHHBBQQKKBBHHRRRRHHBBQQKKBBHHRR'
+
+a = Game(4,"white",5)
+s =a.defineStrategy(boardStart)
+print(s)
