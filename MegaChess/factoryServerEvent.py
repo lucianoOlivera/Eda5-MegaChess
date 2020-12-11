f68172e681f631504cfc16e9fffaf83ae79960d1
Luciano Olivera <oliveralucianof@gmail.com>
2020-12-11 12:55:32 -0300

    rectruturing factory server event code

--- MegaChess/factoryServerEvent.py
+++ MegaChess/factoryServerEvent.py
@@ -2,16 +2,16 @@ from serverEvent import (
     AskChallenge,
     GameOver,
     UpdateUserList,
-    YourTurn)
-
+    YourTurn
+    )
 
 class FactoryServerEvent(object):
     @classmethod
     def get_event(self,event,response):
         if event is 'update_user_list':
-             UpdateUserList(response).run()
+            return UpdateUserList(response).run()
         elif event is 'gameover':
-            GameOver(response).run()
+            return GameOver(response).run()
         elif event is 'ask_challenge':
             return AskChallenge(response).run()
         elif event is 'your_turn':
