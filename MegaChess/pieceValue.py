5822fba9a697447498b00ae16f700cd43b1cbca8
Luciano Olivera <oliveralucianof@gmail.com>
2020-12-11 13:08:01 -0300

    value documentacion

--- MegaChess/pieceValue.py
+++ MegaChess/pieceValue.py
@@ -1,21 +1,29 @@
-from const import Rook,Pawn,King,Knight,Queen,Bishop,Empty
-
+from const import(
+    KING,
+    ROOK,
+    QUEEN,
+    PAWN,
+    BISHOP,
+    KNIGHT,
+    EMPTY)
 
 class PieceValue(object):
     def value(self, name):
+        """
+        :param name: El nombre de la pieza
+        :return: El valor de la pieza
+        """
         if name is "p" or name is 'P':
-            return Pawn
+            return PAWN
         elif name is "h" or name is 'H':
-            return Knight
+            return KNIGHT
         elif name is "q" or name is 'Q':
-            return Queen
+            return QUEEN
         elif name is "k" or name is 'K':
-            return King
+            return KING
         elif name is "r" or name is 'R':
-            return Rook
+            return ROOK
         elif name is "b" or name is 'B':
-            return Bishop
+            return BISHOP
         elif name is " ":
-            return Empty
-
-
+            return EMPTY
