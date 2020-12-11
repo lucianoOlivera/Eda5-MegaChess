8f45f1f4d50e20b02d4567b270e1ad1b36f4e048
Luciano Olivera <oliveralucianof@gmail.com>
2020-12-11 13:04:33 -0300

    highScore and moveScore documentation

--- MegaChess/piecesStrategy.py
+++ MegaChess/piecesStrategy.py
@@ -1,4 +1,11 @@
-from const import King,Rook,Queen,Pawn,Bishop,Knight,Empty
+from const import (
+    KING,
+    ROOK,
+    QUEEN,
+    PAWN,
+    BISHOP,
+    KNIGHT,
+    EMPTY)
 from pieceValue import PieceValue
 
 
@@ -13,34 +20,54 @@ class PiecesStrategy(object):
         raise NotImplementedError
 
 
+def highScore(listMovesScore):
+    """
+    Resuelve cuál es el movimiento con mayor puntaje
+
+    Devuelve un movimiento
+
+    Parámetro
+    listMovesScore ---> Lista de todos los movimientos del turno
+
+    """
+
+    highScoreMove = 0
+    moveHighScore = []
+    for i,c in enumerate(listMovesScore):
+        for w,j in enumerate(c):
+            if j[4]>highScoreMove:
+                highScoreMove = j[4]
+                moveHighScore = j
+    return moveHighScore
+
+
 class MoveScore():
-    def moveScore(self,board,name):
-        if board.get_name() is 'p' or board.get_name() is 'P':
-            scorePiece = PieceValue().value(name)
-            return Pawn + scorePiece
-        elif board.get_name() is 'h' or board.get_name() is 'H':
-            scorePiece = PieceValue().value(name)
-            return Knight + scorePiece
-        elif board.get_name() is 'q' or board.get_name() is 'Q':
-            scorePiece = PieceValue().value(name)
-            return Queen + scorePiece
-        elif board.get_name() is 'r' or board.get_name() is 'R':
-            scorePiece = PieceValue().value(name)
-            return Rook + scorePiece
-        elif board.get_name() is 'b' or board.get_name() is 'B':
-            scorePiece = PieceValue().value(name)
-            return Bishop + scorePiece
-        elif board.get_name() is 'K' or board.get_name() is 'k':
-            scorePiece = PieceValue().value(name)
-            return Knight + scorePiece
+    def moveScore(self,piece,name):
+        """
+        Resuelve el puntaje del movimiento
 
-    def highScore(self,listMovesScore):
-        highScore = 0
-        moveHighScore = []
-        for i,c in enumerate(listMovesScore):
-            for w,j in enumerate(c):
-               if j[4] > highScore:
-                   highScore = j[4]
-                   moveHighScore = j
-        return moveHighScore
+        Devuelve el puntaje del movimiento
 
+        Parámetro
+        piece----> Pieza que hará el movimiento
+        name----> El nombre de la pieza que comerá en caso que sea enemigo
+
+        """
+        if piece.get_name() is 'p' or piece.get_name() is 'P':
+            scorePiece = PieceValue().value(name)
+            return PAWN + scorePiece
+        elif piece.get_name() is 'h' or piece.get_name() is 'H':
+            scorePiece = PieceValue().value(name)
+            return KNIGHT + scorePiece
+        elif piece.get_name() is 'q' or piece.get_name() is 'Q':
+            scorePiece = PieceValue().value(name)
+            return QUEEN + scorePiece
+        elif piece.get_name() is 'r' or piece.get_name() is 'R':
+            scorePiece = PieceValue().value(name)
+            return ROOK + scorePiece
+        elif piece.get_name() is 'b' or piece.get_name() is 'B':
+            scorePiece = PieceValue().value(name)
+            return BISHOP + scorePiece
+        elif piece.get_name() is 'K' or piece.get_name() is 'k':
+            scorePiece = PieceValue().value(name)
+            return KING + scorePiece
